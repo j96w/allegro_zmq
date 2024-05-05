8a1cc6a00feee3a071d34b6c77a758e8f380a7bd
j96w <j96w@qq.com>
2024-05-05 16:07:08 -0700

    upload

---
+++ grasp/RockScissorsPaper.h
@@ -0,0 +1,13 @@
+#ifndef _ROCKSCISSORSPAPER_H
+#define _ROCKSCISSORSPAPER_H
+
+#include <vector>
+
+// TODO: clean this up
+void MotionRock();
+void MotionScissors();
+void MotionPaper();
+
+void SetTargetQ(std::vector<double> q);
+
+#endif
